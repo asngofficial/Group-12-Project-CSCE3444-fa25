b135cba546522bd8e5565915045fde7e607a3979
MatthewBest53421 <bestmatthew9@gmail.com>
2025-11-11 12:06:42 -0600

    backend bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Group-12-Project-CSCE3444-fa25/',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3002',
+        changeOrigin: true,
+      },
+    },
+  },
 })
